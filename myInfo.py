c3e816ff222b95952013a7d10bbe48c21286497c
AmaniM22 <91150774+AmaniM22@users.noreply.github.com>
2022-01-27 01:02:01 -0500

    Add files via upload

---
+++ myInfo.py
@@ -0,0 +1,33 @@
+#CS175I-01
+#Amani Minaya
+#My info
+name = ("Amani Minaya")
+address = ("11 walnut street, Bloomingdale, New Jersey, 07403")
+number = ("201-656-8777")
+major= ("Computer Science")
+
+print ("My Name is: " + name)
+print ("My address is: " + address)
+print ("My phone number is: " + number)
+print ("I am  majoring in: " + major)
+
+
+
+print (" ")
+#I also decided to  do a little program where the user can input thier own info themselves and it will display it as well.
+print ("Now your turn!")
+#Will ask user for the students name
+name2= input("Enter your name: ")
+print("The students name is " + name2 )
+
+#Will ask the user for thier current address
+address2= input("Enter your address: ")
+print(name2 + "'s address is " + address2)
+
+#Will ask the user for their phone number 
+number2= input ("Enter your phone number: ")
+print(name2 + "'s phone number is "+ number2)
+
+#Will ask user to input the major they are studying 
+major2= input ("Enter your major: ")
+print(name2+ "'s major is " + major2) 
