28dd1ea84000511b0d7a094780691390e56ea58c
AmaniM22 <91150774+AmaniM22@users.noreply.github.com>
2022-03-29 20:35:45 -0400

    Update AvgFromInput.py

--- AvgFromInput.py
+++ AvgFromInput.py
@@ -4,7 +4,6 @@ def main():
     total = 0.0
     count = 0
     random_numbers_file = open('numbers.txt','r')
-#iterate for loop for all lines in 'file'
     for line in random_numbers_file:
             number = float(line)
             count += 1
